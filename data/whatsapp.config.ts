032126da26f77465cf91b7fdd7857239874ef3d1
PERCY TUNCAR <percy.edgar.tuncar@gmail.com>
2026-09-01 01:21:42 -0500

    se actualizaron los klinks de los grupos

--- data/whatsapp.config.ts
+++ data/whatsapp.config.ts
@@ -5,11 +5,11 @@
 import type { CountrySlug } from './countries.config'
 
 export const WHATSAPP_GROUPS: Record<CountrySlug, string> = {
-  peru: 'https://chat.whatsapp.com/JJ1UsJddCcuKJMho1l6mau',
-  chile: 'https://chat.whatsapp.com/Ku57tClqYi98jOEHVLgbXZ',
-  argentina: 'https://chat.whatsapp.com/HsRE3Iw4K8tJaJO0w3fovp',
-  colombia: 'https://chat.whatsapp.com/Ebyp5j4Tcm0AhknIpoA8Sh',
-  brasil: 'https://chat.whatsapp.com/Isc5FaiAzUMJwlV4dHZwQx',
+  peru: 'https://chat.whatsapp.com/EFmFkmSxX3c3NvB6nWxoHH',
+  chile: 'https://chat.whatsapp.com/In4ttT9d9qa8DGo7neWW0D',
+  argentina: 'https://chat.whatsapp.com/GZpppHkAQ0UL4ltIfVgrlD',
+  colombia: 'https://chat.whatsapp.com/DI3CQQwlSHjFEhBeyR8Ior',
+  brasil: 'https://chat.whatsapp.com/H47iywDGxZR3euHBljzo7k',
 }
 
 export function getWhatsAppGroupUrl(countrySlug: CountrySlug): string {
